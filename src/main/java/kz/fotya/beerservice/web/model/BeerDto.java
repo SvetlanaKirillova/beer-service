f8778a459171a5b3ffa96201fb75c5d46999c0fd
SvetlanaKirillova <sveta.kirillova.94@gmail.com>
2024-12-03 00:07:48 +0500

    Add Validation to beerDto

--- src/main/java/kz/fotya/beerservice/web/model/BeerDto.java
+++ src/main/java/kz/fotya/beerservice/web/model/BeerDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -15,15 +19,23 @@ import java.util.UUID;
 @Builder
 public class BeerDto {
 
+    @Null
     private UUID id;
+    @Null
     private Integer version;
 
+    @Null
     private OffsetDateTime createdDate;
+    @Null
     private OffsetDateTime lastModifiedDate;
 
+    @NotBlank
     private String beerName;
+    @NotNull
     private BeerStyle beerStyle;
+    @Positive
     private long upc;
     private Integer quantityOnHand;
+    @Positive
     private BigDecimal price;
 }
